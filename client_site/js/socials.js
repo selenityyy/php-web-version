4c52e68f59b859c056ae7c4b5c34f2d8c4712b61
selenityyy <selenac17@icloud.com>
2026-03-12 14:36:03 -0400

    fixed for the millionth time

--- client_site/js/socials.js
+++ client_site/js/socials.js
@@ -1,4 +1,4 @@
 // goes back to homepage when logo is clicked
 function goHome() {
-    window.location.href = "/php-client-site-ahh/client_site/index.php";
+    window.location.href = "/php-web-version/client_site/index.php";
 }
