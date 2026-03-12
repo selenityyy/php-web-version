ce398604ff32f99e908941ec9520c3ebb0803f7d
selenityyy <selenac17@icloud.com>
2026-03-11 22:58:24 -0400

    im gonna krill this siteeee why u no workkkkkk

--- client_site/js/asa.js
+++ client_site/js/asa.js
@@ -40,5 +40,5 @@ fetchEvents();
 
 // goes back to homepage when logo is clicked
 function goHome() {
-    window.location.href = "/php-client-site/client_site/index.php";
+    window.location.href = "/php-client-site-ahh/client_site/index.php";
 }
